7b2b603a6ec03546d85ee64ca779dc0c02ce2d57
Tanseer Saji <tanseersaji@gmail.com>
2019-03-09 20:50:04 +0530

    Added course details

--- courseList.js
+++ courseList.js
@@ -4,14 +4,31 @@ exports.coursesList = [
         name: "Machine Learning (Basics)",
         thumbUrl: "img/thumb/c1.jpg",
         discription: "Learn industry ready machine learning techniques with practical applications while understanding its core fundamentals.",
-        price: 1000
+        price: 1000,
+        objective: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi inventore, soluta velit blanditiis, neque illum quam ullam eos eius enim consectetur voluptatum iusto ex delectus autem dolorum nobis ad nam.",
+        syllabus: [
+            "Chapter 1",
+            "Chapter 2",
+            "Chapter 3"
+        ],
+        outcomes: [
+            "Outcome 1",
+            "Outcome 2",
+        ]
     },
     {
         cid: 2,
         name: "Neural Networks",
         thumbUrl: "img/thumb/c2.jpg",
         discription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam consequuntur quos culpa odit cum natus, quasi molestiae minus, iure, deleniti facere ipsa nemo debitis porro autem praesentium facilis optio ullam",
-        price: 1000
+        price: 1000,
+        objective: "Coming Soon",
+        syllabus: [
+            "Coming Soon"
+        ],
+        outcomes: [
+            "Coming Soon"
+        ]
     },
     {
         cid: 3,
